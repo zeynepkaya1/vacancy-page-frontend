c041ccb177750aeb4776c15f11b8038db1ee4bb1
Zeynep <1998.ze.ka@gmail.com>
2024-12-25 15:48:11 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}", // Scan these files for class names
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
